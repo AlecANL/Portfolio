b06af26cde1ba606eddcdd41c25521fad174db12
alexei <alexeialvarez4@gmail.com>
2024-01-02 21:54:31 -0600

    feat: complete partial portfolio.

---
+++ src/data/projects.ts
@@ -0,0 +1,70 @@
+export const PROJECTS = [
+    {
+        id: 1,
+        average: 10,
+        name: "DevJobs",
+        description: {
+            en: "Imagine building an attractive job board from scratch. That's the essence of this challenge at Frontend Mentor. Using local data from the data.json file, the goal is to bring an elegant design to life. Filter jobs, explore details with a click and make sure it looks amazing no matter the screen size. It's your canvas, make it yours and let it shine! ✨",
+            es: "Imagina construir un tablero de trabajos atractivo desde cero. Esa es la esencia de este desafío en Frontend Mentor. Utilizando datos locales del archivo data.json, el objetivo es dar vida a un diseño elegante. Filtra trabajos, explora detalles con un clic y asegúrate de que se vea increíble sin importar el tamaño de la pantalla. Es tu lienzo, ¡hazlo tuyo y déjalo brillar! ✨",
+        },
+        smallDescription: {
+            es: "Imagina construir un tablero de trabajos atractivo desde cero. Esa es la esencia de este desafío. Utilizando datos locales del archivo data.json, el objetivo es dar vida a un diseño elegante...",
+            en: "Imagine building an attractive job board from scratch. That's the essence of this challenge. Using local data from the data.json file, the goal is to bring to life an elegant design..."
+        },
+        image: '/projects/devjobs-cover.png',
+        stack: [],
+        repository: "https://github.com",
+        demo: "https://github.com"
+    },
+    {
+        id: 1,
+        average: 10,
+        name: "ClockApp",
+        description: {
+            es: "El proyecto consiste en desarrollar una aplicación de reloj que utilice APIs como World Time, IP Geolocation y Quotes para obtener datos en tiempo real. Los usuarios podrán explorar un diseño interactivo, visualizar la hora y ubicación basadas en su IP, acceder a detalles adicionales en el modo expandido, disfrutar de saludos y fondos personalizados según la hora del día, y generar citas aleatorias de programación con un clic. La aplicación ajustará automáticamente el saludo, el ícono y el fondo según la hora actual. El objetivo es crear una aplicación de reloj sencilla y funcional. ⏰🌞🌙",
+            en: "The project consists of developing a clock application that uses APIs such as World Time, IP Geolocation and Quotes to obtain real-time data. Users will be able to explore an interactive layout, view time and location based on their IP, access additional details in expanded mode, enjoy custom greetings and backgrounds based on the time of day, and generate random scheduling quotes with a click. The application will automatically adjust the greeting, icon and background based on the current time. The goal is to create a simple and functional clock app. ⏰🌞🌙"
+        },
+        smallDescription: {
+            es: "El proyecto consiste en desarrollar una aplicación de reloj que utilice APIs como World Time, IP Geolocation y Quotes para obtener datos en tiempo real...",
+            en: "The project consists of developing a clock application that uses APIs such as World Time, IP Geolocation and Quotes to obtain real-time data..."
+        },
+        image: '/projects/clock-cover.png',
+        stack: [],
+        repository: "https://github.com",
+        demo: "https://github.com"
+    },
+    {
+        id: 1,
+        average: 10,
+        name: "Pomodoro App",
+        description: {
+            es:"Este proyecto implica la creación de una aplicación de temporizador Pomodoro que sigue un diseño específico. La aplicación permite iniciar tres tipos de temporizadores, cambiar la apariencia visual, pausar, iniciar y reiniciar el temporizador, y personalizar cada uno de los temporizadores. Los usuarios podrán configurar temporizadores Pomodoro y de descanso, ajustar la duración, y observar una barra de progreso circular que refleja el avance del temporizador. ⏲️🎨",
+            en: "This project involves creating a Pomodoro timer application that follows a specific design. The application allows to start three types of timers, change the visual appearance, pause, start and restart the timer, and customize each timer. Users will be able to set Pomodoro and rest timers, adjust the duration, and observe a circular progress bar that reflects the timer's progress. ⏲️🎨"
+        },
+        smallDescription: {
+            es: "Este proyecto implica la creación de una aplicación de temporizador Pomodoro que sigue un diseño específico. La aplicación permite iniciar tres tipos de temporizadores, cambiar la apariencia visual, pausar, iniciar y reiniciar el temporizador, y personalizar cada uno de los temporizadores...",
+            en: "This project involves creating a Pomodoro timer application that follows a specific design. The application allows you to start three types of timers, change the visual appearance, pause, start and restart the timer, and customize each of the timers...."
+        },
+        image: '/projects/pomodoro-cover.png',
+        stack: [],
+        repository: "https://github.com",
+        demo: "https://github.com"
+    },
+    {
+        id: 1,
+        average: 10,
+        name: "DevFinder",
+        description: {
+            es: "Este proyecto implica la creación de una aplicación que utiliza la API de GitHub para mostrar perfiles de usuarios. Los usuarios podrán explorar perfiles, realizar búsquedas por nombre de usuario, y cambiar entre temas claro y oscuro. El diseño ofrece una experiencia interactiva, incluyendo estados de hover, y la persistencia del tema seleccionado. La aplicación sigue el principio de mostrar información relevante, manejar situaciones de datos faltantes y proporcionar una interfaz atractiva y funcional.",
+            en: "This project involves creating an application that uses the GitHub API to display user profiles. Users will be able to explore profiles, search by username, and switch between light and dark themes. The design offers an interactive experience, including hover states, and the persistence of the selected theme. The application follows the principle of displaying relevant information, handling missing data situations, and providing an attractive and functional interface."
+        },
+        smallDescription: {
+            es: "Este proyecto implica la creación de una aplicación que utiliza la API de GitHub para mostrar perfiles de usuarios. Los usuarios podrán explorar perfiles, realizar búsquedas por nombre de usuario, y cambiar entre temas claro y oscuro....",
+            en: "This project involves creating an application that uses the GitHub API to display user profiles. Users will be able to explore profiles, search by username, and switch between light and dark themes..."
+        },
+        image: '/projects/devfinder-cover.png',
+        stack: [],
+        repository: "https://github.com",
+        demo: "https://github.com"
+    },
+]
