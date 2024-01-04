3ff03019711e3ab557228357899fd4daaef7d536
alexei <alexeialvarez4@gmail.com>
2024-01-04 14:40:17 -0600

    feat: add sitemap 🏗️

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-    i18n: {
-        defaultLocale: 'en',
-        locales: ['en', 'es'],
-        routing: {
-            prefixDefaultLocale: false, 
-        }
+  site: 'https://www.alec-anl.me/',
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'es'],
+    routing: {
+      prefixDefaultLocale: false
     }
+  },
+  integrations: [sitemap()]
 });
